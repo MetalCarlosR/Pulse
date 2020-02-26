c4a9fe67b734e8357f5958835d98d5d57c7b54a5
MetalCarlos <carome06@ucm.es>
2020-02-26 15:03:14 +0100

    Avanzada la linea de visión

---
+++ Assets/Carlos_Romero/Scripts/Enemigo/LineaDeVison.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineaDeVison : MonoBehaviour
+{
+    public float radioVision;
+
+    [Range(0,360)]
+    public float anguloVision;
+
+    public LayerMask jugador;
+    public LayerMask obstaculo;
+
+    private void Start()
+    {
+        StartCoroutine(CheckTimeFix(0.1f));
+    }
+
+
+    public Vector3 ApuntarAngulo(float angulo)
+    {
+        return new Vector3(Mathf.Sin(angulo * Mathf.Deg2Rad), Mathf.Cos(angulo * Mathf.Deg2Rad), 0);
+    }
+
+    IEnumerator CheckTimeFix(float timeFix)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(timeFix);
+            CheckLineaDeVison();
+        }
+    }
+
+    void CheckLineaDeVison()
+    {
+        Collider2D target = Physics2D.OverlapCircle(transform.position, radioVision,jugador);
+                
+        
+        if (target != null)
+        {
+            Transform targetPos = target.transform;
+            Vector3 dir = (targetPos.position - transform.position).normalized;
+
+            if(Vector3.Angle(transform.up , dir)  < anguloVision / 2){
+                transform.up = dir;
+            }
+
+        }
+       
+    }
+}
---
+++ Assets/Editor/FovEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LineaDeVison))]
+public class FovEditor : Editor
+{
+    private void OnSceneGUI()
+    {
+        LineaDeVison FOV = (LineaDeVison)target;
+        Handles.color = Color.white;
+        Handles.DrawWireArc(FOV.transform.position, Vector3.forward, Vector3.up, 360, FOV.radioVision);
+
+        Vector3 AnguloDer = FOV.ApuntarAngulo(FOV.anguloVision / 2);
+        Vector3 AnguloIz = FOV.ApuntarAngulo(-FOV.anguloVision / 2);
+
+        Handles.color = Color.red;
+        Handles.DrawLine(FOV.transform.position, FOV.transform.position + AnguloDer * FOV.radioVision);
+        Handles.DrawLine(FOV.transform.position, FOV.transform.position + AnguloIz * FOV.radioVision);
+    }
+}
