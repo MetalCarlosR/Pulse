e17b8c467d9d2b481de4208efc5cdc118efce574
EpicRoy16 <61495490+EpicRoy16@users.noreply.github.com>
2020-02-28 12:49:20 +0100

    He avanzado en el prototipo de disparo de mi carpeta, dandole una velocidad a la bala y haciendola aparecer al pulsar un botón.

---
+++ Assets/Rodrigo_Tobas/Scripts/MovimientoBala.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoBala : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float velocidadBala;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(Vector3.up * velocidadBala, ForceMode2D.Impulse);
+           
+        
+    }
+}
