01c9abd8c7856c66b20c046211b3cb8699f4f62f
Alejao03 <alejao03@ucm.es>
2020-02-28 12:38:16 +0100

    Movimiento y mira

---
+++ Assets/Carlos_Tomas/Scripts/Mirar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mirar : MonoBehaviour
+{
+    Vector3 mouseposition;
+    Vector2 direction;
+    void Update()
+    {
+        mouseposition = Input.mousePosition;
+        mouseposition = Camera.main.ScreenToWorldPoint(mouseposition);
+
+        direction = new Vector2(mouseposition.x - transform.position.x, mouseposition.y - transform.position.y);
+
+        transform.up = direction;
+    }
+}
