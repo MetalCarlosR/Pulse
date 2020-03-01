32e0f94f626a7c91314cebebc8e316a8fd2c4f23
rubenglezortiz <61495670+rubenglezortiz@users.noreply.github.com>
2020-03-01 19:03:14 +0100

    DagaBehaviour
    Ya está configurada la daga. Habrá que cambiar alguna cosa de cara a cuando introduzcamos animaciones.

---
+++ Assets/Individuales/Ruben_Gonzalez/Scripts/DagaBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DagaBehaviour : MonoBehaviour
+{
+    BoxCollider2D colliderDaga;
+    private void Start()
+    {
+        colliderDaga = GetComponent<BoxCollider2D>();
+        colliderDaga.enabled = !enabled;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            transform.position += transform.right * 0.2f;
+            colliderDaga.enabled = enabled;
+        }
+        else if (Input.GetKeyUp("space"))
+        {
+            transform.position -= transform.right * 0.2f;
+            colliderDaga.enabled = !enabled;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(collision.gameObject);
+    }
+
+}
