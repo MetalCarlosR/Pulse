6b782c2d2f3e8e3f0660aa341ee4cbee02eb407b
julio.agostini <julio.agostini@cs.ucalgary.ca>
2020-03-04 09:41:19 +0100

    Circulo de pulse funciona

---
+++ Assets/Individuales/Julio_Agostini/Scripts/PulseCirculo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PulseCirculo : MonoBehaviour
+{
+
+
+    private Transform circle;
+    private Vector3 startingSize = new Vector3(0.25f, 0.25f, 1);
+    private Vector3 scale;
+    private float growth = 0.005f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        circle = gameObject.transform;
+        scale = new Vector3();
+        StartCoroutine("Reset");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        circle.localScale = new Vector3(circle.localScale.x + growth, circle.localScale.y + growth, 1);
+    }
+
+
+    private IEnumerator Reset()
+    {
+        Debug.Log("got here");
+        yield return new WaitForSeconds(2);
+        circle.localScale = startingSize;
+        StartCoroutine("Reset");
+    }
+}
