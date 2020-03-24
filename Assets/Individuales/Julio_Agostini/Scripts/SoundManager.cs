3399ea555a725e364950b417fc6640544b860bd3
julio.agostini <julio.agostini@cs.ucalgary.ca>
2020-03-24 11:08:37 -0700

    comenze sound manager

---
+++ Assets/Individuales/Julio_Agostini/Scripts/SoundManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+    public static SoundManager smInstance_;
+
+    [SerializeField]
+    private AudioSource shooting, door, walking, reload;
+
+
+
+    void Awake()
+    {
+        {
+            if (smInstance_ == null)
+            {
+                smInstance_ = this;
+                Debug.Log("SoundManager Set");
+            }
+            else if (smInstance_ != this)
+            {
+                Destroy(gameObject);
+            }
+            DontDestroyOnLoad(this);
+        }
+    }
+
+
+    public void PlayShooting()
+    {
+        shooting.Play();
+    }
+
+    public void PlayDoor()
+    {
+        door.Play();
+    }
+    public void PlayReload()
+    {
+        reload.Play();
+    }
+    public void Playwalking()
+    {
+        walking.Play();
+    }
+
+    public void StopWalking()
+    {
+        walking.Stop();
+    }
+
+}
