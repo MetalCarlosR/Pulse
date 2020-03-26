d298e4045d68fbd9fcd00bf2b7a24292d558bd29
MetalCarlosR <MetalCarlosR@users.noreply.github.com>
2020-03-26 18:34:15 +0100

    sonido antiguo comentado

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -22,17 +22,15 @@ public class SoundManager : MonoBehaviour
 
     void Awake()
     {
+        if (smInstance_ == null)
         {
-            if (smInstance_ == null)
-            {
-                smInstance_ = this;
-            }
-            else if (smInstance_ != this)
-            {
-                Destroy(gameObject);
-            }
-            DontDestroyOnLoad(this);
+            smInstance_ = this;
         }
+        else if (smInstance_ != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(this);
     }
 
     public void PlaySound(Audio audio)
@@ -40,3 +38,74 @@ public class SoundManager : MonoBehaviour
         audioPool[Convert.ToInt32(audio)].Play();
     }
 }
+
+//private AudioSource door, walking, enemyDeath;
+//private List<AudioSource> shots;
+//[SerializeField]
+//private int numOfShots;
+//[SerializeField]
+//private GameObject shot;
+//void Awake()
+//{
+//    {
+//        if (smInstance_ == null)
+//        {
+//            smInstance_ = this;
+//            shots = new List<AudioSource>(numOfShots);
+//            for (int i = 0; i < numOfShots; i++)
+//            {
+//                CreateShot();
+//            }
+//            Debug.Log(shots);
+
+//        }
+//        else if (smInstance_ != this)
+//        {
+//            Destroy(gameObject);
+//        }
+//        DontDestroyOnLoad(this);
+//    }
+//}
+
+//private void CreateShot()
+//{
+//    GameObject newShot;
+//    newShot = Instantiate(shot);
+//    shots.Add(newShot.GetComponent<AudioSource>());
+//}
+
+//public void PlayShooting()
+//{
+//    bool shot = false;
+//    int i = 0;
+//    while (!shot)
+//    {
+//        if (!shots[i].isPlaying)
+//        {
+//            shots[i].Play();
+//            shot = true;
+//        }
+//        i++;
+//    }
+//}
+
+//public void PlayDoor()
+//{
+//    door.Play();
+//}
+//public void Playwalking()
+//{
+//    walking.Play();
+//}
+
+//public void StopWalking()
+//{
+//    walking.Stop();
+//}
+
+//public void PlayEnemyDeath()
+//{
+//    enemyDeath.Play();
+//}
+
+//}
