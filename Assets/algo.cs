114b997377854c8ec70aa60953138f8f60d9918b
MetalCarlosR <MetalCarlosR@users.noreply.github.com>
2020-04-06 20:14:47 +0200

    pruebas navmesh y creación de mapas

---
+++ Assets/algo.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine.AI;
+using UnityEngine;
+
+public class algo : MonoBehaviour
+{
+    NavMeshAgent navMesh;
+    [SerializeField]
+    private Transform player;
+
+    bool algob = true;
+    void Start()
+    {
+        navMesh = GetComponent<NavMeshAgent>();
+        navMesh.updateRotation = false;
+        navMesh.updateUpAxis = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(algob)   navMesh.SetDestination(player.position);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            algob = false;
+            navMesh.ResetPath();
+        }
+        if (Input.GetKeyUp(KeyCode.Space)) algob = true; 
+    }
+}
