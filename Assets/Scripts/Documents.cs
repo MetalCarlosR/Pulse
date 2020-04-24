56e23085d1afe9bc235c8df423b8607d0fb906c9
Rubén González Ortiz <rubgon08@ucm.es>
2020-04-24 18:10:14 +0200

    Introducción de los documentos y arreglo de la interfaz

---
+++ Assets/Scripts/Documents.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Documents : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerController>())
+            if (GameManager.gmInstance_ != null)
+                GameManager.gmInstance_.ChangeScene("FinishScene");
+    }
+}
