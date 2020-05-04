6c7f39e4a66b2f0e71ab86b25d0dca6fc83a4a1b
Rubén González Ortiz <rubgon08@ucm.es>
2020-05-04 16:49:24 +0200

    Menú Implementado

---
+++ Assets/Scripts/MenuSound.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSound : MonoBehaviour
+{
+    private AudioSource audio;
+    public void Play()
+    {
+        audio.Play();
+    }
+}
