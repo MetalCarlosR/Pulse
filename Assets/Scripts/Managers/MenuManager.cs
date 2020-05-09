6f650a1e8b838e7a4c22b45ed697bdf90fb9879c
Rubén González Ortiz <rubgon08@ucm.es>
2020-05-09 19:56:28 +0200

    Menu Update

---
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject initialButtons, settings, howToPlay, backgroundGroup, back; /*exit, , audioSettings, back;*/
+    private bool ini;
+    private void Start()
+    {
+        ini = true;
+    }
+    public void Settings()
+    {
+        initialButtons.SetActive(false);
+        settings.SetActive(true);
+        back.SetActive(true);
+    }
+
+    public void Back()
+    {
+        if (ini)
+        {
+            settings.SetActive(false);
+            initialButtons.SetActive(true);
+            back.SetActive(false);
+        }
+        else
+        {
+            settings.SetActive(true);
+            howToPlay.SetActive(false);
+            backgroundGroup.SetActive(true);
+            ini = true;
+        }
+    }
+
+    public void HowToPlay()
+    {
+        howToPlay.SetActive(true);
+        settings.SetActive(false);
+        back.SetActive(true);
+        backgroundGroup.SetActive(false);
+        ini = false;
+    }
+}
