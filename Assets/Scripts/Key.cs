a3dc88a229e1278170b5e0c0418f11ed3bc029a1
MetalCarlosR <MetalCarlosR@users.noreply.github.com>
2020-05-27 00:35:13 +0200

    Creada la interacción con la llave para acabar el nivel

---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    private GameObject text;
+
+    private void Start()
+    {
+        text = transform.GetChild(0).gameObject;
+        text.SetActive(false);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerController>()) text.SetActive(true);
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (GameManager.gmInstance_ != null && Input.GetButtonDown("Use") && collision.gameObject.GetComponent<PlayerController>())
+        {
+            GameManager.gmInstance_.ChangeScene("FinishScene");
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerController>()) text.SetActive(false);
+    }
+}
